856757982e39702c50eb9cbdde04d437e9bcd332
TrainerA@hexaware.com <TrainerA@hexaware.com>
2019-02-26 12:12:32 +0530

    Inheritance coding

---
+++ New folder/InheritanceTest1.java
@@ -0,0 +1,22 @@
+class A {
+	void dis1() {
+		System.out.println("dis1() method");
+	}
+}
+class B extends A{
+	void dis2() {
+		System.out.println("dis2() mthod");
+	}
+}
+class InheritanceTest1 {
+	public static void main(String args[]){
+	A obj1 = new A();
+	obj1.dis1();
+	B obj2 = new B();
+	obj2.dis2();
+	obj2.dis1();
+	}
+}
+
+
+
---
+++ New folder/InheritanceTest2.java
@@ -0,0 +1,24 @@
+class Bike {
+	void speed() {
+		System.out.println("60km/hr");
+	}
+}
+class Honda extends Bike {
+	void color() {
+		System.out.println("Black");
+	}
+}
+class Pulsar extends Bike {
+	void color() {
+		System.out.println("Red");
+	}
+}
+class InheritanceTest2 {
+	public static void main(String args[]){
+	Honda hh = new Honda();	hh.color();	hh.speed();
+	Pulsar pu = new Pulsar();	pu.color();	pu.speed();
+	}
+}
+
+
+
